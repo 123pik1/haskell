2fb486ce98f6c448fd3b50680b21e14bd38bed5c
123pik1 <s197736@student.pg.edu.pl>
2024-12-07 11:07:34 +0100

    Refactor code in zadanie1.hs to improve readability and maintainability

--- zadanie1.hs
+++ zadanie1.hs
@@ -1,27 +1,30 @@
 import Data.List (group, sort, maximumBy)
 import Data.Ord (comparing)
 
--- Function to generate all Pythagorean triplets (a, b, c) such that a + b + c = x
+-- znadjuje a,b,c i sprawdza czy tworzą one trójkąt prostokatny oraz sprawdza czy w sumie dają x, zwraca listę trójek
 pythagoreanTriplets :: Int -> [(Int, Int, Int)]
 pythagoreanTriplets x = [(a, b, c) | a <- [1..x], b <- [a..x], let c = x - a - b, a^2 + b^2 == c^2, a + b + c == x]
 
--- Function to count the number of triplets for each x ≤ n
+-- generuje x, a potem zlicza dla każdego wygenerowanego x trójki takie że a+b+c=x i tworzące trójkąt prostokątny, 
+-- zwraca liczbę x i ilość trójek w tuplu
 countTriplets :: Int -> [(Int, Int)]
 countTriplets n = [(x, length (pythagoreanTriplets x)) | x <- [1..n], not (null (pythagoreanTriplets x))]
 
--- Function to find the maximum count and return all x values with this count
+-- zwraca listę x dla których jest najwięcej trójek
 maxTriplets :: Int -> [Int]
 maxTriplets n = map fst ( filter ((== maxCount) . snd) tripletCounts)
   where
     tripletCounts = countTriplets n
     maxCount = maximum (map snd tripletCounts)
 
--- Main function to get the result
+-- zwraca listę x dla których jest najwięcej trójek
 findMaxTriplets :: Int -> [Int]
 findMaxTriplets n = maxTriplets n
 
--- Example usage
+-- testy
 main :: IO ()
 main = do
   let n = 40
   print (findMaxTriplets n)
+  let n1 = 200
+  print (findMaxTriplets n1)
